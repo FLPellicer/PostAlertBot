8e5d9931e8c48eee9e9d418a0ada9e9d08456ccc
unknown <federicopellicer19@gmail.com>
2020-09-23 21:46:11 -0500

    <Added functionality to see if submission already existis in current inbox messages>

--- postAlertBot.py
+++ postAlertBot.py
@@ -13,14 +13,31 @@ pmTitle = "Match found for " + keyword
 
 pmBody = "A new post including your keywords was found! Here is the link: "
 
+messagesBodyUrl = []
+
+def sumbission_already_sent(url):
+    for message in messagesBodyUrl:
+        if url in message:
+            print('message repeated, will not send')
+            return bool(True)
+        else: 
+            return bool(False)
+        
+
+for message in reddit.inbox.messages(limit=10):
+    if pmTitle in message.subject:
+        messagesBodyUrl.append(message.body)
+
 for submission in subreddit.new(limit=10):
     print("Title: ", submission.title)
     
     if keyword in submission.title.lower():
         url = url + submission.permalink
-        pmBody = pmBody + url
-        reddit.redditor("PossibleFailure").message(pmTitle, pmBody)
+        is_sent = sumbission_already_sent(url)
+        if not is_sent:
+            print('Messages will be sent')
+            pmBody = pmBody + url
+            reddit.redditor("PossibleFailure").message(pmTitle, pmBody)
         
 
-    
 
